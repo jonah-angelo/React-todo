ebe89b59429cf49c35876bc7d4d755160079b1d8
jonah-angelo <jonah.angelo@gmail.com>
2023-06-05 19:27:14 -0700

    delete function working

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -17,6 +17,24 @@ function App() {
     setNewItem("")
   }
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return { ...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id )
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className='new-item-form'>
@@ -31,10 +49,14 @@ function App() {
         {todos.map (todo => {
           return <li key={todo.id}>
             <label>
-            <input type='checkbox' checked={todo.completed} />
+            <input type='checkbox' 
+            checked={todo.completed} 
+            onChange={e => toggleTodo(todo.id, e.target.checked)}
+            />
             {todo.title}
           </label>
-            <button className='btn btn-danger'>Delete</button>
+            <button onClick={() => deleteTodo(todo.id)}
+            className='btn btn-danger'>Delete</button>
           </li>
         })}
       </ul>
